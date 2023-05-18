a3b25941e76988f1bbe81a454921d34c55154003
Fawad Tahir <114586343+FawadTahir123@users.noreply.github.com>
2023-05-18 17:17:40 +0500

    Avsilsbiity check

--- src/components/AvailabiltyCheck.jsx
+++ src/components/AvailabiltyCheck.jsx
@@ -1,90 +1,112 @@
 import React, { useEffect, useState } from "react";
 import Navbar from "./Home";
-import { Switch, Card, Button } from "antd";
+import { Switch, Card, Button, Drawer, Space } from "antd";
 import { Link } from "react-router-dom";
 import Header from "../components/Navbar";
 
-
 function AvailabiltyCheck() {
   const userID = localStorage.getItem("id");
+  const [bloodGroup, setBloodGroup] = useState("");
   const [ischecked, setChecked] = useState(false);
-  const [disable, setDisable] = useState(false)
+  const [disable, setDisable] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [patient, setPatient] = useState([]);
+  const [isBoxChecked, setisChecked] = useState(null);
 
 
-  const checkForevents  = () =>{
-    const event = "hello"
-    if(event === "hello"){
-      setDisable(true)
-    }else{
-      setDisable(false)
-    }
-  }
+  // const getAvailability = async () => {
+  //   try {
+  //     const res = await fetch(
+  //       `http://127.0.0.1:5000/api/get-user-availability/${userID}`,
+  //       {
+  //         method: "GET",
+  //       }
+  //     );
+  //     const result = await res.json();
+  //     if (result.data[0].availability === "Available") {
+  //       setChecked(true);
+  //       setOpen(true)
+  //       // checkForevents()
 
-  const getAvailability = async () => {
+  //     } else {
+  //       setChecked(false);
+  //       // checkForevents()
+  //     }
+  //   } catch (e) {
+  //     console.log("error", e);
+  //   }
+  // };
+
+  const getBloodGroup = async () => {
     try {
       const res = await fetch(
-        `http://127.0.0.1:5000/api/get-user-availability/${userID}`,
+        `http://127.0.0.1:5000/api/get-donor-bloodgroup/${userID}`,
         {
           method: "GET",
         }
       );
       const result = await res.json();
-      if (result.data[0].availability === "Available") {
-        setChecked(true);
-        // checkForevents()
-
-      } else {
-        setChecked(false);
-        // checkForevents()
-      }
+      setBloodGroup(result.data[0].blood_group);
     } catch (e) {
       console.log("error", e);
     }
   };
-
-
-
   const onChange = async (checked) => {
     if (checked) {
       try {
+        setChecked('')
         setChecked(true);
         const res = await fetch(
-          `http://127.0.0.1:5000/api/update-availability/${userID}`,
+          `http://127.0.0.1:5000/api/patient-request-for-donor`,
           {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-              id: userID,
-              availability: "Available",
+              blood_group: bloodGroup,
             }),
           }
         );
         const result = await res.json();
-        console.log(result);
+        setPatient(result.data);
+        console.log(result.data);
+        setOpen(true);
       } catch (e) {
         console.log("error", e);
       }
     } else {
+      setOpen(false);
+      setChecked(false);
+    }
+  };
+  const onClose = () => {
+    setOpen(false);
+    setChecked(false)
+  };
+
+  const handelCheckbox = async(e) => {
+    const { value, checked } = e.target;
+    console.log(value,'value', checked,"checked") 
+    if(checked){
+      setisChecked(value);
       try {
-        setChecked(false);
         const res = await fetch(
-          `http://127.0.0.1:5000/api/update-availability/${userID}`,
+          `http://127.0.0.1:5000/api/get-single-request-data/${value}`,
           {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({
-              availability: "not_available",
-            }),
+            method: "GET",
           }
         );
         const result = await res.json();
-        console.log(result);
+          console.log(result.data);
       } catch (e) {
         console.log("error", e);
       }
+    } else {
+      setisChecked()
     }
+    
   };
 
+console.log(isBoxChecked, "ischecked");
   const divStyle = {
     margin: "auto",
     width: "50%",
@@ -109,15 +131,17 @@ function AvailabiltyCheck() {
     width: "auto",
   };
   useEffect(() => {
-    getAvailability();
-  }, [ischecked]);
+    getBloodGroup();
+  }, []);
 
   return (
-    <body className='check-event'>
-      <Header/>
+    <body className="check-event">
+      <Header />
       <div className="container" style={divStyle}>
         <div style={{ textAlign: "center" }}>
-          <h1 style={{ marginTop: "20px", color:'#fff',fontWeight: "bold" }}>let us know when you are available</h1>
+          <h1 style={{ marginTop: "20px", color: "#fff", fontWeight: "bold" }}>
+            let us know when you are available
+          </h1>
         </div>
         <div style={buttonStyle}>
           <Button style={{ color: "white", background: "#ff5348" }}>
@@ -125,16 +149,58 @@ function AvailabiltyCheck() {
           </Button>
         </div>
         <div style={divStyleforSwitch} className="d-flex">
-          <p style={{ fontWeight: "bold", color:'#fff'}}>Set Availability</p>
-{
-  !disable ? 
-  <Switch checked={ischecked}  onChange={onChange} /> : <Switch checked={false} disabled={true} onChange={onChange} />
-}
+          <p style={{ fontWeight: "bold", color: "#fff" }}>Set Availability</p>
+          {!disable ? (
+            <Switch checked={ischecked} onChange={onChange} />
+          ) : (
+            <Switch checked={false} disabled={true} onChange={onChange} />
+          )}
         </div>
       </div>
+      <Drawer
+        title="Patient Available"
+        placement="right"
+        width={500}
+        onClose={onClose}
+        open={open}
+        extra={
+          <Space>
+            <Button onClick={onClose}>Cancel</Button>
+            <Button type="primary" onClick={onClose}>
+              OK
+            </Button>
+          </Space>
+        
+        }
+        footer={
+          <Button style={{width:'100%', justifyContent:'center'}}>Submit</Button>
+        }
+      >
+        {patient.map((patientData) => {
+          return (
+            <div className="d-flex" style={{ justifyContent: "space-evenly" }}>
+              <input
+                type="checkbox"
+                value={patientData.id}
+                checked={isBoxChecked == patientData.id ? true : false}
+                onChange={(e) => handelCheckbox(e)}
+              />
+              <Card
+                title={patientData.first_name + " " + patientData.last_name}
+                bordered={true}
+                style={{
+                  width: 300,
+                }}
+              >
+                <p>{patientData.blood_group}</p>
+                <p>{patientData.required_date}</p>
+                <p>Card content</p>
+              </Card>
+            </div>
+          );
+        })}
+      </Drawer>
     </body>
-    
-  
   );
 }
 
